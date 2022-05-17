365a8e8c9aa7496a87fdcca578572c3c6bd4e3d8
YuCheng <69775014+Yu-C-UTS@users.noreply.github.com>
2022-05-17 21:09:15 +0800

    New FireFlickerScript

---
+++ Assets/Scripts/FireFlicker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlicker : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] lights;
+
+    private Vector3[] initialLightPositions;
+    private Vector2[] lightPerlinOffset;
+
+    public float FlickerDistance = 0.1f;
+    public float FlickerFrequency = 1;
+
+    private void Start() 
+    {
+        initialLightPositions = new Vector3[lights.Length];
+        lightPerlinOffset = new Vector2[lights.Length];
+        for(int i = 0; i < lights.Length; i++)
+        {
+            initialLightPositions[i] = lights[i].position;
+            lightPerlinOffset[i] = new Vector2(Random.value, Random.value);
+        }    
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < lights.Length; i++)
+        {
+            lights[i].position = initialLightPositions[i] + new Vector3(Mathf.PerlinNoise(Time.time * FlickerFrequency, - lightPerlinOffset[i].x) - 0.5f, Mathf.PerlinNoise(Time.time * FlickerDistance, - lightPerlinOffset[i].y) - 0.5f, 0);
+        }
+    }
+}
